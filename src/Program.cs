c077b4c11df0a02f675341c9b7e070a72f95009e
Emi <lunaemilio2003@gmail.com>
2025-12-22 18:15:50 -0300

    refactor: on websockets connection

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 using Fleck;
+using ws_api.Services;
 
 var server = new WebSocketServer("ws://0.0.0.0:8181");
 
@@ -20,4 +21,29 @@ server.Start(ws =>
     };
 });
 
-WebApplication.CreateBuilder(args).Build().Run();
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSingleton<ChatWebSocketHandler>();
+
+var app = builder.Build();
+
+app.UseWebSockets();
+
+app.MapControllers();
+
+app.Map("/ws", async context =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        var manager = context.RequestServices.GetRequiredService<ChatWebSocketHandler>();
+        var socket = await context.WebSockets.AcceptWebSocketAsync();
+        await manager.HandleAsync(socket);
+    }
+    else
+    {
+        context.Response.StatusCode = 400;
+    }
+});
+
+app.Run();
