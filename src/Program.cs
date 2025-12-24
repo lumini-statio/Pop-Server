42269db848148de6b07752e256dfa379d220f34f
Emi <lunaemilio2003@gmail.com>
2025-12-23 23:12:11 -0300

    feature: central config to manage communication

--- src/Program.cs
+++ src/Program.cs
@@ -1,55 +1,27 @@
-using Fleck;
-using Microsoft.EntityFrameworkCore;
-using ws_api.Database;
+using ws_api.Hubs;
 using ws_api.Services;
-
-var server = new WebSocketServer("ws://0.0.0.0:8181");
-
-var wsConnections = new List<IWebSocketConnection>();
-
-server.Start(ws =>
-{
-    ws.OnOpen = () =>
-    {
-        wsConnections.Add(ws);
-    };
-
-    ws.OnMessage = message =>
-    {
-        foreach (var conn in wsConnections)
-        {
-            conn.Send(message);
-        }
-    };
-});
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddSingleton<ChatWebSocketHandler>();
-builder.Services.AddDbContext<AppDbContext>(options =>
+builder.Services.AddSignalR();
+builder.Services.AddSingleton<RoomService>();
+
+builder.Services.AddCors(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.AddDefaultPolicy(policy =>
+    {
+        policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .SetIsOriginAllowed(_ => true)
+            .AllowCredentials();
+    });
 });
 
 var app = builder.Build();
 
-app.UseWebSockets();
+app.UseCors();
 
-app.MapControllers();
-
-app.Map("/ws", async context =>
-{
-    if (context.WebSockets.IsWebSocketRequest)
-    {
-        var manager = context.RequestServices.GetRequiredService<ChatWebSocketHandler>();
-        var socket = await context.WebSockets.AcceptWebSocketAsync();
-        await manager.HandleAsync(socket);
-    }
-    else
-    {
-        context.Response.StatusCode = 400;
-    }
-});
+app.MapHub<VoiceHub>("/voice");
 
 app.Run();
